9c68992a53a97e7b9067e916a45d76b8bed7bdf5
Ryan Seto <ryan.seto11@gmail.com>
2025-11-05 01:14:57 -0500

    Fixed letter casing issue

--- rummikub-discord/server/src/server.ts
+++ rummikub-discord/server/src/server.ts
@@ -3,7 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
-import authRouter from './Routes/auth';
+import authRouter from './routes/auth';
 
 
 dotenv.config();
